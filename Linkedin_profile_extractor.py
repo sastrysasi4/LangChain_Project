e60c0e5fbc6f38f7bf673168887aca04f3128529
sasidhar-guggilla <sastrysasi4@gmail.com>
2023-10-25 15:34:20 +0530

    langchain commit v1

---
+++ Linkedin_profile_extractor.py
@@ -0,0 +1,35 @@
+## Here we are building a Linkedin profile summarizer using LangChain, OpenAI and Serp_API
+
+
+from dotenv import load_dotenv
+from langchain.prompts import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+load_dotenv()
+# print(os.getenv("OPENAI_API_KEY"))
+
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from third_parties.linkedin import scrape_linkedin_profile
+
+if __name__=='__main__':
+
+    print("hello langchain!")
+
+    linkedin_profile_url = linkedin_lookup_agent(name = 'Eden Marco')
+
+    summary_template = """ give the information {information} about a person from i want you to create
+    1. a short summary
+    2. two interesting facts about them """
+
+    summary_prompt_template = PromptTemplate(input_variables=['information'],template=summary_template)
+
+    llm = ChatOpenAI(temperature=0,model_name='gpt-3.5-turbo')
+
+    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+
+    likedin_data = scrape_linkedin_profile(linkedin_profile_url)
+
+    print(chain.run(information = likedin_data))
+
+
+
---
+++ README.md
@@ -0,0 +1,12 @@
+# LangChain Project
+
+In this project we are going to see the power of Langchain and its capabilities. It enables applications that can combine LLMs with other sources of computation or knowledge, such as prompt templates, external data, and other tools.
+
+Langchain can help with both tasks by using LLMs to analyze and generate text, and chains to connect LLMs with other components that can provide additional information or functionality.
+
+## Features
+
+- LangChain
+- OpenAI
+- Proxy curl API
+- Serp API
